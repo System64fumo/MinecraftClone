26f835350de04ddd34c31b59f132ba65080a0e1e
system64fumo <system64fumo@protonmail.com>
2025-02-07 11:16:35 +0200

    Misc improvements
    Fixed player position being incorrect (Flipped)
    Made the more verbose debug text print only if needed.
    Set default player position and angle to be looking towards the chunks to help me benchmark my changes.

--- src/player.c
+++ src/player.c
@@ -3,25 +3,25 @@
 void process_keyboard_movement(const Uint8* key_state, Player* player, float delta_time) {
 	const float PI = 3.14159265358979323846f;
 	if (key_state[SDL_SCANCODE_W]) {
-		player->z += cos(player->yaw * PI / 180.0f) * player->speed * delta_time;
-		player->x += -sin(player->yaw * PI / 180.0f) * player->speed * delta_time;
-	}
-	if (key_state[SDL_SCANCODE_S]) {
 		player->z -= cos(player->yaw * PI / 180.0f) * player->speed * delta_time;
 		player->x -= -sin(player->yaw * PI / 180.0f) * player->speed * delta_time;
 	}
-	if (key_state[SDL_SCANCODE_A]) {
-		player->x += cos(player->yaw * PI / 180.0f) * player->speed * delta_time;
-		player->z += sin(player->yaw * PI / 180.0f) * player->speed * delta_time;
+	if (key_state[SDL_SCANCODE_S]) {
+		player->z += cos(player->yaw * PI / 180.0f) * player->speed * delta_time;
+		player->x += -sin(player->yaw * PI / 180.0f) * player->speed * delta_time;
 	}
-	if (key_state[SDL_SCANCODE_D]) {
+	if (key_state[SDL_SCANCODE_A]) {
 		player->x -= cos(player->yaw * PI / 180.0f) * player->speed * delta_time;
 		player->z -= sin(player->yaw * PI / 180.0f) * player->speed * delta_time;
 	}
+	if (key_state[SDL_SCANCODE_D]) {
+		player->x += cos(player->yaw * PI / 180.0f) * player->speed * delta_time;
+		player->z += sin(player->yaw * PI / 180.0f) * player->speed * delta_time;
+	}
 	if (key_state[SDL_SCANCODE_SPACE]) {
-		player->y -= player->speed * delta_time;
+		player->y += player->speed * delta_time;
 	}
 	if (key_state[SDL_SCANCODE_LSHIFT] || key_state[SDL_SCANCODE_RSHIFT]) {
-		player->y += player->speed * delta_time;
+		player->y -= player->speed * delta_time;
 	}
 }
