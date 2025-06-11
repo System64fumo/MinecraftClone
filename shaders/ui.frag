696a9d03a6695c68b4c0fa9688303d80396222fa
system64fumo <system64fumo@protonmail.com>
2025-06-11 17:21:37 +0300

    Fixups
    Add dynamic UI elements support.
    Fix text rendering.
    Fix text spacing.
    Add drop shadow to text.

--- shaders/ui.frag
+++ shaders/ui.frag
@@ -2,9 +2,29 @@
 precision mediump float;
 out vec4 FragColor;
 in vec2 TexCoord;
+flat in uint TexId;
 
 uniform sampler2D uiTexture;
 
 void main() {
-	FragColor = texture(uiTexture, TexCoord);
+	vec4 texColor = texture(uiTexture, TexCoord);
+	if (texColor.a > 0.0) {
+		FragColor = texColor;
+		return;
+	}
+
+	if (TexId == 3u) {
+		// Calculate shadow only if main texture is transparent
+		vec2 texSize = vec2(textureSize(uiTexture, 0));
+		vec2 shadowCoord = TexCoord + vec2(-1.0, -1.0) / texSize;
+		vec4 shadowTex = texture(uiTexture, shadowCoord);
+		
+		if (shadowTex.a > 0.0) {
+			FragColor = vec4(0.0, 0.0, 0.0, 0.5 * shadowTex.a);
+		} else {
+			FragColor = vec4(0.0);
+		}
+	} else {
+		FragColor = vec4(0.0);
+	}
 }
---
+++ src/graphics/text.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include "gui.h"
+
+uint8_t font_data[255][2] = {
+	[0 ... 254] = {8, 8},
+	[' '] = {4, 8},
+	['!'] = {2, 8},
+	['a' ... 'z'] = {6, 8},
+	['A' ... 'Z'] = {6, 8},
+	['0' ... '9'] = {6, 8},
+	['o'] = {6, 6},
+	['i'] = {2, 8},
+	['l'] = {3, 8},
+};
+
+uint16_t get_text_length(char* ptr) {
+	uint16_t offset = 0;
+	while (*ptr != '\0') {
+		offset += font_data[*ptr++][0] * 2;
+	}
+	return offset;
+}
+
+void draw_char(char chr, uint16_t x, uint16_t y) {
+	uint8_t index_x, index_y;
+	uint8_t col = chr % 16;
+	uint8_t row = chr / 16;
+	index_x = col * 16;
+	index_y = row * 16;
+
+	ui_element_t char_element = {
+		.x = x - (8 - font_data[chr][0]),
+		.y = y - (8 - font_data[chr][1]),
+		.width = font_data[chr][0],
+		.height = font_data[chr][1],
+		.tex_x = index_x,
+		.tex_y = index_y + (8 - font_data[chr][1]) * 2,
+		.tex_width = font_data[chr][0] * 2, // 2x because the atlas is 128x128 but we expect 256x256
+		.tex_height = font_data[chr][1] * 2,
+		.texture_id = font_textures
+	};
+	add_ui_element(&char_element);
+}
+
+void draw_text(char* ptr, uint16_t x, uint16_t y) {
+	uint8_t char_index = 0;
+	uint16_t offset = 9;
+	while (*ptr != '\0') {
+		draw_char(*ptr, x + offset, y);
+		offset += font_data[*ptr][0] * 2;
+		ptr++;
+		char_index++;
+	}
+}
